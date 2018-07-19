5cda38fa6bafa23caf6b8bfc82602fa10690033f
jan.hudak <hudakjan83@gmail.com>
2018-07-19 11:07:04 +0200

    social

---
+++ src/main/java/sk/hudak/prco/configuration/SocialConfig.java
@@ -0,0 +1,42 @@
+package sk.hudak.prco.configuration;
+
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.social.UserIdSource;
+//import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
+//import org.springframework.social.config.annotation.EnableSocial;
+//import org.springframework.social.config.annotation.SocialConfigurer;
+//import org.springframework.social.connect.ConnectionFactoryLocator;
+//import org.springframework.social.connect.UsersConnectionRepository;
+//import org.springframework.social.connect.mem.InMemoryUsersConnectionRepository;
+//import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+
+@Configuration
+//@EnableSocial
+public class SocialConfig /*implements SocialConfigurer*/ {
+
+//    @Override
+//    public void addConnectionFactories(ConnectionFactoryConfigurer cfConfig, Environment env) {
+//        cfConfig.addConnectionFactory(new FacebookConnectionFactory(
+//                env.getProperty("facebook.appKey"),
+//                env.getProperty("facebook.appSecret")));
+//    }
+//
+//    @Override
+//    public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+//        InMemoryUsersConnectionRepository repository = new InMemoryUsersConnectionRepository(connectionFactoryLocator);
+//
+//        //TODO continue https://github.com/spring-projects/spring-social-samples/blob/master/spring-social-quickstart/src/main/java/org/springframework/social/quickstart/config/SocialConfig.java
+////        repository.setConnectionSignUp(new SimpleConnectionSignUp);
+//        return repository;
+//    }
+//
+//    @Override
+//    public UserIdSource getUserIdSource() {
+//        return new UserIdSource() {
+//            @Override
+//            public String getUserId() {
+//                return SecurityContext.getCurrentUser().getId();
+//            }
+//        };
+//    }
+}
