24600829f47f04cd23954c391c942f02a045261d
jan.hudak <hudakjan83@gmail.com>
2018-09-04 14:50:41 +0200

    added name atribute

--- src/main/java/sk/hudak/prco/dto/product/ProductUnitDataDto.java
+++ src/main/java/sk/hudak/prco/dto/product/ProductUnitDataDto.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 public class ProductUnitDataDto implements DtoAble {
     private Long id;
 
+    private String name;
     private Unit unit;
     private BigDecimal unitValue;
     private Integer unitPackageCount;
