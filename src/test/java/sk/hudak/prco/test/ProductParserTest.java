2f802eac23715b28d0ffba0772423bfda990e8b1
jan.hudak <hudakjan83@gmail.com>
2019-02-25 08:53:10 +0100

    update TODO list

--- src/test/java/sk/hudak/prco/test/ProductParserTest.java
+++ src/test/java/sk/hudak/prco/test/ProductParserTest.java
@@ -2,7 +2,7 @@ package sk.hudak.prco.test;
 
 import sk.hudak.prco.builder.SearchUrlBuilder;
 import sk.hudak.prco.builder.impl.SearchUrlBuilderImpl;
-import sk.hudak.prco.eshop.DrMaxProductParser;
+import sk.hudak.prco.eshop.KidMarketProductParser;
 import sk.hudak.prco.parser.impl.UnitParserImpl;
 import sk.hudak.prco.ssl.PrcoSslManager;
 import sk.hudak.prco.utils.UserAgentDataHolder;
@@ -91,9 +91,10 @@ public class ProductParserTest {
 //                .parseProductUpdateData("https://www.maxikovy-hracky.cz/pampers-active-baby-3-midi-174ks?zmena_meny=EUR"));
 //
 //        // KidMarket
-//        System.out.println(new KidMarketProductParser(unitParser, userAgentDataHolder, searchUrlBuilder)
+        System.out.println(new KidMarketProductParser(unitParser, userAgentDataHolder, searchUrlBuilder)
 //                .parseUrlsOfProduct("pampers"));
-//                .parseNewProductInfo("https://kidmarket.sk/jednorazove-plienky/176-pampers-active-baby-dry-vel6-extra-large-56ks-4015400736394.html?search_query=pampers+5&results=46"));
+//                .parseNewProductInfo("https://kidmarket.sk/jednorazove-plienky/183-pampers-active-baby-4-maxi-pack-58ks.html"));
+                .parseProductUpdateData("https://kidmarket.sk/jednorazove-plienky/183-pampers-active-baby-4-maxi-pack-58ks.html"));
 //                .parseProductUpdateData("https://kidmarket.sk/jednorazove-plienky/183-pampers-active-baby-4-maxi-pack-58ks.html"));
 //                .parseProductUpdateData("https://kidmarket.sk/jednorazove-plienky/177-pampers-active-baby-5-giant-pack-64ks.html?search_query=pampers&results=64"));
 //
@@ -115,9 +116,9 @@ public class ProductParserTest {
 //                .parseProductUpdateData("https://www.hornbach.sk/shop/Zahradny-domcek-Duramax-Colossus-plechovy/6147837/artikel.html"));
 
 // Dx max
-        System.out.println(new DrMaxProductParser(unitParser, userAgentDataHolder, searchUrlBuilder)
+//        System.out.println(new DrMaxProductParser(unitParser, userAgentDataHolder, searchUrlBuilder)
 //                .parseUrlsOfProduct("pampers/ 5"));
-                .parseNewProductInfo("https://www.drmax.sk/pampers-plienky-s4p-active-baby-monthly-box-152/"));
+//                .parseNewProductInfo("https://www.drmax.sk/pampers-plienky-s4p-active-baby-monthly-box-152/"));
 //                .parseNewProductInfo("https://www.drmax.sk/pampers-baby-wipes-fresh-clean-64ks/"));
 //                .parseNewProductInfo("https://www.drmax.sk/pampers-mega-maxi-132/"));
 //                .parseProductUpdateData("https://www.drmax.sk/pampers-mega-maxi-132/"));
