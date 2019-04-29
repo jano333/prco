d8f6ec0ae9f82f89d55f5072ea100017ac31b14e
jan.hudak <jan.hudak@wirecard.com>
2019-04-29 16:02:15 +0200

    refactor

--- src/main/java/sk/hudak/prco/utils/Validate.java
+++ src/main/java/sk/hudak/prco/utils/Validate.java
@@ -1,5 +1,6 @@
 package sk.hudak.prco.utils;
 
+import lombok.NonNull;
 import org.apache.commons.lang3.StringUtils;
 import sk.hudak.prco.exception.PrcoRuntimeException;
 import sk.hudak.prco.exception.RequiredParameterException;
@@ -14,40 +15,40 @@ public class Validate {
         //no instance
     }
 
-    public static void notNull(Object value, String parameterName) {
+    public static void notNull(Object value, @NonNull String parameterName) {
         if (value == null) {
             throw new RequiredParameterException(parameterName);
         }
     }
 
-    public static void notNullNotEmpty(String value, String parameterName) {
+    public static void notNullNotEmpty(String value, @NonNull String parameterName) {
         notNull(value, parameterName);
         if (StringUtils.isBlank(value)) {
             throw new RequiredParameterException(parameterName);
         }
     }
 
-    public static void notNullNotEmpty(String[] value, String parameterName) {
+    public static void notNullNotEmpty(String[] value, @NonNull String parameterName) {
         notNull(value, parameterName);
         if (value.length == 0) {
             throw new RequiredParameterException(parameterName);
         }
     }
 
-    public static void notNullNotEmpty(Collection<?> value, String parameterName) {
+    public static void notNullNotEmpty(Collection<?> value, @NonNull String parameterName) {
         notNull(value, parameterName);
         if (value.isEmpty()) {
             throw new RequiredParameterException(parameterName);
         }
     }
 
-    public static void atLeastOneIsNotNull(Long[] values, String parameterName) {
+    public static void atLeastOneIsNotNull(Long[] values, @NonNull String parameterName) {
         if (values == null || values.length == 0) {
             throw new PrcoRuntimeException("At least one parameter " + parameterName + " is required");
         }
     }
 
-    public static void notNegativeAndNotZeroValue(int value, String parameterName) {
+    public static void notNegativeAndNotZeroValue(int value, @NonNull String parameterName) {
         if (value < ONE) {
             throw new PrcoRuntimeException("Value is zero or negative for " + parameterName + " .");
         }
