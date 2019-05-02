6c329acedd9e3a0b42d25eba28affdef7a882567
jan.hudak <jan.hudak@wirecard.com>
2019-05-02 08:55:58 +0200

    added javdoc

--- src/main/java/sk/hudak/prco/manager/AddingNewProductManager.java
+++ src/main/java/sk/hudak/prco/manager/AddingNewProductManager.java
@@ -4,22 +4,28 @@ import sk.hudak.prco.api.EshopUuid;
 
 public interface AddingNewProductManager {
 
+    /**
+     * @param productsUrl list of new product URL's
+     */
     void addNewProductsByUrl(String... productsUrl);
 
     /**
      * Vyhlada produkty s danym klucovym slovom pre konkretny eshop a ulozi ich do tabulky NEW_PRODUCT.
      *
      * @param eshopUuid     eshop identifikator
-     * @param searchKeyWord
+     * @param searchKeyWord search key word
      */
     void addNewProductsByKeywordForEshop(EshopUuid eshopUuid, String searchKeyWord);
 
     /**
      * Vyhlada produkty s danym klucovym slovom pre vsetky eshopy a ulozi ich do tabulky NEW_PRODUCT.
      *
-     * @param searchKeyWord
+     * @param searchKeyWord search key word
      */
     void addNewProductsByKeywordForAllEshops(String searchKeyWord);
 
+    /**
+     * @param searchKeyWords search key words
+     */
     void addNewProductsByKeywordsForAllEshops(String... searchKeyWords);
 }
