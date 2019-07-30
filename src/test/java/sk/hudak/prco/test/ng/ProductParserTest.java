72b0253a5d28a01a8a4e9e9b0d51a434da158f6f
jan.hudak <jan.hudak@wirecard.com>
2019-07-30 14:58:16 +0200

    new test class

--- src/test/java/sk/hudak/prco/test/ng/ProductParserTest.java
+++ src/test/java/sk/hudak/prco/test/ng/ProductParserTest.java
@@ -4,10 +4,14 @@ import sk.hudak.prco.api.EshopUuid;
 import sk.hudak.prco.builder.SearchUrlBuilder;
 import sk.hudak.prco.builder.SearchUrlBuilderImpl;
 import sk.hudak.prco.dto.internal.ProductNewData;
+import sk.hudak.prco.dto.internal.ProductUpdateData;
 import sk.hudak.prco.eshop.AlzaProductParser;
+import sk.hudak.prco.eshop.PilulkaProductParser;
 import sk.hudak.prco.exception.PrcoRuntimeException;
 import sk.hudak.prco.parser.EshopProductsParser;
+import sk.hudak.prco.parser.EshopUuidParser;
 import sk.hudak.prco.parser.UnitParser;
+import sk.hudak.prco.parser.impl.EnumImplEshopUuidParserImpl;
 import sk.hudak.prco.parser.impl.UnitParserImpl;
 import sk.hudak.prco.ssl.PrcoSslManager;
 import sk.hudak.prco.utils.UserAgentDataHolder;
@@ -19,6 +23,7 @@ public class ProductParserTest {
     private final UserAgentDataHolder userAgentDataHolder;
     private final UnitParser unitParser;
     private final SearchUrlBuilder searchUrlBuilder;
+    private final EshopUuidParser eshopUuidParser;
 
     public ProductParserTest() {
         PrcoSslManager.INSTANCE.init();
@@ -26,14 +31,28 @@ public class ProductParserTest {
         userAgentDataHolder.init();
         unitParser = new UnitParserImpl();
         searchUrlBuilder = new SearchUrlBuilderImpl();
+        eshopUuidParser = new EnumImplEshopUuidParserImpl();
     }
 
     public static void main(String[] args) {
         ProductParserTest test = new ProductParserTest();
 
-        System.out.println(test.parseUrlsOfProduct(EshopUuid.MALL, "pampers"));
+//        System.out.println(test.parseUrlsOfProduct(EshopUuid.MALL, "pampers"));
+//        System.out.println(test.parseProductNewData("https://www.pilulka24.sk/pampers-activebaby-giant-pack-3-midi-90"));
+        System.out.println(test.parseProductUpdateData("https://www.pilulka.sk/pampers-active-baby-mqp-5-junior-124ks-utierky-zadarmo"));
+    }
 
-        System.out.println(test.parseProductNewData("https://www.pilulka24.sk/pampers-activebaby-giant-pack-3-midi-90"));
+    private EshopProductsParser getParserForEshop(EshopUuid eshopUuid) {
+        switch (eshopUuid) {
+            case ALZA:
+                return new AlzaProductParser(unitParser, userAgentDataHolder, searchUrlBuilder);
+            case PILULKA:
+                return new PilulkaProductParser(unitParser, userAgentDataHolder, searchUrlBuilder);
+
+
+            default:
+                throw new PrcoRuntimeException("Pridaj implementaciu do testu pre eshop " + eshopUuid);
+        }
     }
 
     private List<String> parseUrlsOfProduct(EshopUuid eshopUuid, String keyword) {
@@ -41,19 +60,11 @@ public class ProductParserTest {
     }
 
     private ProductNewData parseProductNewData(String productUrl) {
-//        eshopUuidParser.parseEshopUuid(productUrl)
-        return null;
+        return getParserForEshop(eshopUuidParser.parseEshopUuid(productUrl)).parseProductNewData(productUrl);
     }
 
-    private EshopProductsParser getParserForEshop(EshopUuid eshopUuid) {
-        switch (eshopUuid) {
-            case ALZA:
-                return new AlzaProductParser(unitParser, userAgentDataHolder, searchUrlBuilder);
-
-
-            default:
-                throw new PrcoRuntimeException("Pridaj implementaciu");
-        }
+    private ProductUpdateData parseProductUpdateData(String productUrl) {
+        return getParserForEshop(eshopUuidParser.parseEshopUuid(productUrl)).parseProductUpdateData(productUrl);
     }
 
 }
