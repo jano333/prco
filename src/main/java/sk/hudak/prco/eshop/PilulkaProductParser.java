6aa6bafd3d6dcf84bb1cf3ab29c2bd3ad342e9c9
jan.hudak <jan.hudak@wirecard.com>
2019-07-30 14:58:03 +0200

    eshop parser fix

--- src/main/java/sk/hudak/prco/eshop/PilulkaProductParser.java
+++ src/main/java/sk/hudak/prco/eshop/PilulkaProductParser.java
@@ -1,5 +1,6 @@
 package sk.hudak.prco.eshop;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -10,6 +11,8 @@ import sk.hudak.prco.api.ProductAction;
 import sk.hudak.prco.builder.SearchUrlBuilder;
 import sk.hudak.prco.parser.UnitParser;
 import sk.hudak.prco.parser.impl.JSoupProductParser;
+import sk.hudak.prco.utils.ConvertUtils;
+import sk.hudak.prco.utils.JsoupUtils;
 import sk.hudak.prco.utils.UserAgentDataHolder;
 
 import java.math.BigDecimal;
@@ -20,6 +23,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static java.util.Optional.ofNullable;
 import static sk.hudak.prco.api.EshopUuid.PILULKA;
 import static sk.hudak.prco.utils.ConvertUtils.convertToBigDecimal;
 import static sk.hudak.prco.utils.JsoupUtils.getFirstElementByClass;
@@ -72,34 +76,51 @@ public class PilulkaProductParser extends JSoupProductParser {
     @Override
     protected Optional<String> parseProductNameFromDetail(Document documentDetailProduct) {
         Element first = documentDetailProduct.select("div[id=product-info] > form > div > h1 > span[itemprop=name]").first();
-        if (first == null) {
-            return Optional.empty();
+        if (first != null) {
+            return Optional.ofNullable(first.text());
         }
-        return Optional.ofNullable(first.text());
+        return ofNullable(documentDetailProduct.select("span[class='product-detail__header pr-3']").first())
+                .map(Element::text);
     }
 
     @Override
     protected boolean isProductUnavailable(Document documentDetailProduct) {
-        return null == documentDetailProduct.select("input[value=Kúpiť]").first();
+        boolean isProductUnavailable = null == documentDetailProduct.select("input[value=Kúpiť]").first();
+        if (isProductUnavailable) {
+            isProductUnavailable = null == documentDetailProduct.select("#js-add-to-cart-first").first();
+        }
+        return isProductUnavailable;
     }
 
     @Override
     protected Optional<BigDecimal> parseProductPriceForPackage(Document documentDetailProduct) {
         Element first = documentDetailProduct.select("strong[id=priceNew]").first();
-        if (first == null) {
-            return Optional.empty();
+        if (first != null) {
+            String substring = first.text().substring(0, first.text().length() - 2);
+            return Optional.of(convertToBigDecimal(substring));
         }
-        String substring = first.text().substring(0, first.text().length() - 2);
-        return Optional.of(convertToBigDecimal(substring));
+
+        first = documentDetailProduct.select("span[class='fs-28 mb-3 text-primary fwb']").first();
+        return Optional.ofNullable(first)
+                .map(Element::text)
+                .map(text -> StringUtils.removeEnd(text, " €"))
+                .map(ConvertUtils::convertToBigDecimal);
     }
 
     @Override
     protected Optional<String> parseProductPictureURL(Document documentDetailProduct) {
         Element first = documentDetailProduct.select("#pr-img-carousel > ul > li > a > img").first();
-        if (first == null) {
-            return Optional.empty();
+        if (first != null) {
+            return Optional.of(getEshopUuid().getProductStartUrl() + "/" + first.attr("src"));
         }
-        return Optional.of(getEshopUuid().getProductStartUrl() + "/" + first.attr("src"));
+        first = documentDetailProduct.select("div[class='product-detail__images'] > picture > a > img").first();
+        if (first == null) {
+            first = documentDetailProduct.select("div[class='product-detail__images w-100 js-carousel-item'] > picture > a > img").first();
+        }
+        return ofNullable(first)
+                .map(JsoupUtils::dataSrcAttribute)
+                .map(text -> text.substring(1))
+                .map(text -> getEshopUuid().getProductStartUrl() + "/"+ text);
     }
 
     @Override
